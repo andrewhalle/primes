877b92575e1e88a60af4f136a8daaa7a476edc21
Randy Barlow <randy@electronsweatshop.com>
2022-08-10 00:32:00 -0400

    Vary the size of the inputs for the benchmarks
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- benches/all.rs
+++ benches/all.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use primes::{
     eratosthenes::{og, skip_2},
@@ -7,58 +7,43 @@ use primes::{
     Primes,
 };
 
-// The largest prime number we will search for for eratosthenes-based algorithms.
-const LARGEST_PRIME_TO_SEARCH: u64 = 821641;
-
-// The index of LARGEST_PRIME_TO_SEARCH in an array of prime numbers.
-const LARGEST_PRIME_INDEX: usize = 65536;
-
-fn live_first_primes(c: &mut Criterion) {
-    c.bench_function("live_first_primes", |b| {
-        b.iter(|| {
-            let _: Vec<u64> = LiveFirstPrimes::new().take(LARGEST_PRIME_INDEX).collect();
-        })
-    });
-}
-
-fn live_second_primes(c: &mut Criterion) {
-    c.bench_function("live_second_primes", |b| {
-        b.iter(|| {
-            let _: Vec<u64> = LiveSecondPrimes::new().take(LARGEST_PRIME_INDEX).collect();
-        })
-    });
-}
-
-fn sieve_og(c: &mut Criterion) {
-    c.bench_function("sieve_og", |b| {
-        b.iter(|| {
-            let _ = og::primes(LARGEST_PRIME_TO_SEARCH);
-        })
-    });
-}
-
-fn sieve_skip_2(c: &mut Criterion) {
-    c.bench_function("sieve_skip_2", |b| {
-        b.iter(|| {
-            let _ = skip_2::primes(LARGEST_PRIME_TO_SEARCH);
-        })
-    });
-}
-
-fn test_division(c: &mut Criterion) {
-    c.bench_function("test_division", |b| {
-        b.iter(|| {
-            let _: Vec<u32> = Primes::new().take(LARGEST_PRIME_INDEX).collect();
-        })
-    });
+fn bench_em(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Primes");
+    // For each tuple, the first number is the largest prime number we will search for for
+    // eratosthenes-based algorithms and the second number is the index of that same prime
+    // in an array of prime numbers.
+    for i in [(821641, 65536), (9999991, 664579), (499999993, 26355867)].iter() {
+        group.bench_with_input(BenchmarkId::new("division", i.1), i, |b, i| {
+            b.iter(|| {
+                let _: Vec<u32> = Primes::new().take(i.1).collect();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("live_first_prime", i.1), i, |b, i| {
+            b.iter(|| {
+                let _: Vec<u64> = LiveFirstPrimes::new().take(i.1).collect();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("live_second_prime", i.1), i, |b, i| {
+            b.iter(|| {
+                let _: Vec<u64> = LiveSecondPrimes::new().take(i.1).collect();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("sieve_og", i.0), i, |b, i| {
+            b.iter(|| {
+                let _ = og::primes(i.0);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("sieve_skip_2", i.0), i, |b, i| {
+            b.iter(|| {
+                let _ = skip_2::primes(i.0);
+            })
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(
     all,
-    live_first_primes,
-    live_second_primes,
-    test_division,
-    sieve_og,
-    sieve_skip_2
+    bench_em,
 );
 criterion_main!(all);
